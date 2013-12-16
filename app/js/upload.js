15e98646d7411124f96843477b4df5e013930750
Ilias Ismanalijev <hello@illyism.com>
2013-12-16 18:29:37 +0100

    update

--- app/js/upload.js
+++ app/js/upload.js
@@ -2,10 +2,30 @@
 $(function () {
     $('#fileupload').fileupload({
         dataType: 'json',
+        add: function (e, data) {
+        	$('.progress .bar').css(
+                'width',
+                "0%"
+            );
+           data.submit();
+        },
         done: function (e, data) {
         	var result = data.result;
             var zip = result.zip;
             location.href = zip;
+            $(".progress").addClass("success");
+            $('.progress .bar').css(
+                'width',
+                "100%"
+            );
+        },
+        progressall: function (e, data) {
+            var progress = parseInt(data.loaded / data.total * 100, 10);
+            console.log(progress+"%");
+            $('.progress .bar').css(
+                'width',
+                progress + '%'
+            );
         }
     });
 });
